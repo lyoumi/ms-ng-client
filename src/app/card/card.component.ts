c4684d63e916cc597e91fcedb993f003b5bac5d7
Artem Nikulin <artem.nikulin@davita.com>
2020-05-02 19:24:34 +0300

    Added null-check

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -20,8 +20,9 @@ export class CardComponent {
   title: string = 'Hello world'
 
   arr: User[] = [
-    { username: 'Vasya', age: 23, skills: [{title: 'Java', score: 9}, {title: 'SQL', score: 8}] },
-    { username: 'Petya', age:20, skills: [{title: 'Angular', score: 6}, {title: 'HTTP', score: 7}] }
+      { username: 'Vasya', age: 23, skills: [{title: 'Java', score: 9}, {title: 'SQL', score: 8}] },
+      { username: 'Petya', age:20, skills: [{title: 'Angular', score: 6}, {title: 'HTTP', score: 7}] },
+      null
     ]
 
 }
